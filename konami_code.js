6e0ea3ea97f4fc542b40fd945cee5766f3aeed49
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-22 06:07:04 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -29,36 +29,6 @@ function init() {
     }
   }
 }
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
 
-function init() {
-  let index = 0;
-  const body = document.querySelector('body');
-  
-  function keyDownHandler(e){
-    const key = e.key;
-    
-    if(key === codes[index]){
-      index++;
-      
-      if(index === codes.length){
-        alert("Konami Code!!!!!");
-      }
-    } else {
-      index = 0;
-    }
-  }
-}
-body.addEventListener('keydown', keyDownHandler(e));
+body.addEventListener('keydown', keyDownHandler);
 
